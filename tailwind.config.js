8e777891ce24905db8ca8b348bbcf33b1c8b42b2
YoussefMossallem <ymslm120@gmail.com>
2026-04-07 23:04:55 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ export default {
         },
       },
       fontFamily: {
-        sans: ["Inter", "system-ui", "sans-serif"],
+       sans: ["IBM Plex Sans Arabic", "sans-serif"],
       },
     },
   },
